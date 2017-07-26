ccd689d45a59541ad2dca95d7427104eadb9b210
Sergei Kosov <kosichlancer@gmail.com>
2017-07-26 21:18:25 +0300

    26.07.2017

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    // тогглы для меню
+    $('.bottomHamburger').click(function () {
+        console.error('123');
+        $('.headerBottom nav').slideToggle();
+
+    })
+
+});
