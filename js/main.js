8d22d0c443f58ca59f44ab06924aa0a9a14231ce
Sergei Kosov <kosichlancer@gmail.com>
2017-07-27 01:43:02 +0300

    range slider

--- js/main.js
+++ js/main.js
@@ -1,4 +1,34 @@
+
+
+
+
 $(document).ready(function () {
+
+    var $document   = $(document),
+        $inputRange = $('input[type="range"]');
+
+    // Example functionality to demonstrate a value feedback
+    function valueOutput(element) {
+        var value = element.value,
+            output = element.parentNode.getElementsByClassName('val-number')[0];
+        output.innerHTML = value;
+    }
+
+    for (var i = $inputRange.length - 1; i >= 0; i--) {
+        valueOutput($inputRange[i]);
+    };
+    $document.on('input', 'input[type="range"]', function(e) {
+        valueOutput(e.target);
+
+
+
+    });
+    // end
+
+    $inputRange.rangeslider({
+        polyfill: false
+    });
+
 
     // тогглы для меню
     $('.bottomHamburger').click(function () {
@@ -6,5 +36,6 @@ $(document).ready(function () {
         $('.headerBottom nav').slideToggle();
 
     })
+
 
 });
